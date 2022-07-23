027c94cada0c1ca3bd50ce1f6faefd50cd4110f7
覃有义 <amz27149@dingtalk.com>
2022-07-23 10:44:22 +0800

    fix: lint staged

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((prevCount) => prevCount + 2)}>
+        <button onClick={() => setCount((prevCount) => prevCount + 222)}>
           count is {count}
         </button>
         <p>
